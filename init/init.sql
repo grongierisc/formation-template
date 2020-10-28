612999aa9f7fbc9f3c4fc5fee8957ccae2732040
grongierisc <guillaume.rongier@intersystems.com>
2020-10-28 14:19:52 +0100

    Update init postgres

--- init/init.sql
+++ init/init.sql
@@ -1,1 +1,15 @@
 CREATE SCHEMA demodata;
+
+CREATE TABLE public.formation (
+	id int8 NULL,
+	nom varchar NULL,
+	salle varchar NULL
+);
+
+INSERT INTO public.formation
+(id, nom, salle)
+VALUES(1, 'formation1', 'salle1');
+
+INSERT INTO public.formation
+(id, nom, salle)
+VALUES(2, 'formation2', 'salle2');
