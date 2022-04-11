885c07f6cb7a74bcf92eff1dc6fc57cecdab5a89
grongierisc <guillaume.rongier@intersystems.com>
2022-04-11 14:48:17 +0000

    register

---
+++ misc/register.py
@@ -0,0 +1,4 @@
+iris.cls("Grongier.PEX.Utils").RegisterComponent("bs.ServiceCSV","ServiceCSV","/irisdev/app/src/python/",1,"Python.ServiceCSV")
+iris.cls("Grongier.PEX.Utils").RegisterComponent("bo.FileOperation","FileOperation","/irisdev/app/src/python/",1,"Python.FileOperation")
+iris.cls("Grongier.PEX.Utils").RegisterComponent("bo.IrisOperation","IrisOperation","/irisdev/app/src/python/",1,"Python.IrisOperation")
+iris.cls("Grongier.PEX.Utils").RegisterComponent("bp.Router","Router","/irisdev/app/src/python/",1,"Python.Router")
